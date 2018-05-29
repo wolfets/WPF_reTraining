32f6c6699736e993cc86555f5e552ccc365905db
STD <wolfets@free.fr>
2018-05-29 10:18:08 +0200

    mise en place d'un autre service (DataService2)

---
+++ MvvmLight1/Model/DataService2/IDataService2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MvvmLight1.Model
+{
+    public interface IDataService2
+    {
+        void GetData(Action<DataItem2, Exception> callback);
+    }
+}
---
+++ MvvmLight1/UC/UserControlBinding.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MvvmLight1.UC
+{
+    /// <summary>
+    /// Logique d'interaction pour UserControlBinding.xaml
+    /// </summary>
+    public partial class UserControlBinding : UserControl
+    {
+        public UserControlBinding()
+        {
+            InitializeComponent();
+        }
+    }
+}
