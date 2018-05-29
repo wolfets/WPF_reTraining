f3b26ef89e64e5de4aba19a65a4446d31c4f381e
STD <wolfets@free.fr>
2018-05-29 10:30:06 +0200

    replacement projet UC
    replacement projet UC

---
+++ SeperateWPFUserControl_Dll/UserControl1.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace SeperateWPFUserControl_Dll
+{
+    public partial class UserControl1 : UserControl
+    {
+        #region Label DP
+
+        /// <summary>
+        /// Gets or sets the Label which is displayed next to the field
+        /// </summary>
+        public String Label
+        {
+            get { return (String)GetValue(LabelProperty); }
+            set { SetValue(LabelProperty, value); }
+        }
+
+        /// <summary>
+        /// Identified the Label dependency property
+        /// </summary>
+        public static readonly DependencyProperty LabelProperty =
+            DependencyProperty.Register("Label", typeof(string),
+              typeof(UserControl1), new PropertyMetadata(""));
+
+        #endregion
+
+        #region Value DP
+
+        /// <summary>
+        /// Gets or sets the Value which is being displayed
+        /// </summary>
+        public object Value
+        {
+            get { return (object)GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+
+        /// <summary>
+        /// Identified the Label dependency property
+        /// </summary>
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.Register("Value", typeof(object),
+              typeof(UserControl1), new PropertyMetadata(null));
+
+        #endregion
+
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+    }
+}
