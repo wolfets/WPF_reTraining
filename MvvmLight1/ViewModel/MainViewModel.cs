f199cf187ca10859b7e725ee172de758703cae37
STD <wolfets@free.fr>
2018-06-08 11:04:44 +0200

    task await en cours

--- MvvmLight1/ViewModel/MainViewModel.cs
+++ MvvmLight1/ViewModel/MainViewModel.cs
@@ -143,7 +143,15 @@ namespace MvvmLight1.ViewModel
         }
 
 
-
+        public async Task<string> ReadFile(string filepath)
+        {
+            await {
+                using (var reader = new StreamReader(filepath))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+        }
 
         ////public override void Cleanup()
         ////{
