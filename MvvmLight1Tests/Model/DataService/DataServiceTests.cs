8d8880d702e359b2e43a3df4b1c8b8aacb44d759
STD <wolfets@free.fr>
2018-07-02 12:56:24 +0200

    tests

---
+++ MvvmLight1Tests/Model/DataService/DataServiceTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MvvmLight1.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvvmLight1.Model.Tests
+{
+    [TestClass()]
+    public class DataServiceTests
+    {
+        [TestMethod()]
+        public void GetDataTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
